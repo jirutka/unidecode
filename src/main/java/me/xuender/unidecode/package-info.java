e1b63ccb79e39f51de8872f4cfa4344dee730f50
Jakub Jirutka <jakub@jirutka.cz>
2014-04-23 23:46:01 +0200

    Fork and update POM

--- src/main/java/me/xuender/unidecode/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @see <a href="https://github.com/xuender/unidecode">Unidecode at github.com</a>
- * @author <a href="mailto:xuender@gmail.com">ender</a>
- *
- */
-package me.xuender.unidecode;
