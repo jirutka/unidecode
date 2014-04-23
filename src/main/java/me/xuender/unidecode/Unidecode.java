03cb2c8deccd2f8a39635ef55d021eea48c2b130
Jakub Jirutka <jakub@jirutka.cz>
2014-04-23 23:47:03 +0200

    Fix codestyle

--- src/main/java/me/xuender/unidecode/Unidecode.java
+++ src/main/java/me/xuender/unidecode/Unidecode.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package me.xuender.unidecode;
 
@@ -14,105 +14,105 @@ import java.util.regex.Pattern;
  * <p>
  * Transliterate Unicode string to a valid 7-bit ASCII String.
  * </p>
- * 
+ *
  * @see <a href="https://pypi.python.org/pypi/Unidecode">Python`s Unidecode</a>
  * @see <a href="https://github.com/xuender/unidecode">Unidecode at
  *      github.com</a>
  * @author <a href="mailto:xuender@gmail.com">ender</a>
  */
 public final class Unidecode {
-	/**
-	 * Array to cache already loaded maps.
-	 */
-	private static final String[][] cache = new String[256][];
+    /**
+     * Array to cache already loaded maps.
+     */
+    private static final String[][] CACHE = new String[256][];
 
-	/**
-	 * Transliterate an Unicode object into an ASCII string.
-	 * 
-	 * @param str
-	 *            Unicode String to transliterate.
-	 * @return ASCII string.
-	 */
-	public static String decode(final String str) {
-		if (str == null) {
-			return "";
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-			int codepoint = str.codePointAt(i);
-			// Basic ASCII
-			if (codepoint < 0x80) {
-				sb.append(c);
-				continue;
-			}
-			// Characters in Private Use Area and above are ignored
-			if (codepoint > 0xffff)
-				continue;
-			int section = codepoint >> 8; // Chop off the last two hex digits
-			int position = codepoint % 256; // Last two hex digits
-			String[] table = getCache(section);
-			if (table != null && table.length > position) {
-				sb.append(table[position]);
-			}
-		}
-		return sb.toString().trim();
-	}
+    /**
+     * Transliterate an Unicode object into an ASCII string.
+     *
+     * @param str
+     *            Unicode String to transliterate.
+     * @return ASCII string.
+     */
+    public static String decode(final String str) {
+        if (str == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int codepoint = str.codePointAt(i);
+            // Basic ASCII
+            if (codepoint < 0x80) {
+                sb.append(c);
+                continue;
+            }
+            // Characters in Private Use Area and above are ignored
+            if (codepoint > 0xffff)
+                continue;
+            int section = codepoint >> 8; // Chop off the last two hex digits
+            int position = codepoint % 256; // Last two hex digits
+            String[] table = getCache(section);
+            if (table != null && table.length > position) {
+                sb.append(table[position]);
+            }
+        }
+        return sb.toString().trim();
+    }
 
-	private static String[] getCache(int section) {
-		String[] ret = cache[section];
-		if (ret == null) {
-			InputStream inStream = null;
-			try {
-				inStream = Unidecode.class.getResourceAsStream(String.format(
-						"/data/X%03x", section));
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(inStream));
-				String line = null;
-				ret = new String[256];
-				int i = 0;
-				while ((line = reader.readLine()) != null) {
-					ret[i] = line;
-					i++;
-				}
-				cache[section] = ret;
-			} catch (Exception e) {
-				// No match: ignore this character and carry on.
-				cache[section] = new String[] {};
-			} finally {
-				if (inStream != null) {
-					try {
-						inStream.close();
-					} catch (IOException e) {
-					}
-				}
-			}
-		} else if (ret.length == 0) {
-			return null;
-		}
-		return ret;
-	}
+    private static String[] getCache(int section) {
+        String[] ret = CACHE[section];
+        if (ret == null) {
+            InputStream inStream = null;
+            try {
+                inStream = Unidecode.class.getResourceAsStream(String.format(
+                        "/data/X%03x", section));
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inStream));
+                String line = null;
+                ret = new String[256];
+                int i = 0;
+                while ((line = reader.readLine()) != null) {
+                    ret[i] = line;
+                    i++;
+                }
+                CACHE[section] = ret;
+            } catch (Exception e) {
+                // No match: ignore this character and carry on.
+                CACHE[section] = new String[] {};
+            } finally {
+                if (inStream != null) {
+                    try {
+                        inStream.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        } else if (ret.length == 0) {
+            return null;
+        }
+        return ret;
+    }
 
-	/**
-	 * Transliterate Unicode string to a initials.
-	 * 
-	 * @param str
-	 *            Unicode String to initials.
-	 * @return String initials.
-	 */
-	public static String initials(final String str) {
-		if (str == null) {
-			return "";
-		}
-		StringBuilder sb = new StringBuilder();
-		Pattern p = Pattern.compile("^\\w|\\s+\\w");
-		Matcher m = p.matcher(decode(str));
-		while (m.find()) {
-			sb.append(m.group().replaceAll(" ", ""));
-		}
-		return sb.toString();
-	}
+    /**
+     * Transliterate Unicode string to a initials.
+     *
+     * @param str
+     *            Unicode String to initials.
+     * @return String initials.
+     */
+    public static String initials(final String str) {
+        if (str == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Pattern p = Pattern.compile("^\\w|\\s+\\w");
+        Matcher m = p.matcher(decode(str));
+        while (m.find()) {
+            sb.append(m.group().replaceAll(" ", ""));
+        }
+        return sb.toString();
+    }
 
-	private Unidecode() {
-	}
+    private Unidecode() {
+    }
 }
--- src/test/java/me/xuender/unidecode/UnidecodeTest.java
+++ src/test/java/me/xuender/unidecode/UnidecodeTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package me.xuender.unidecode;
 
@@ -10,115 +10,115 @@ import org.junit.Test;
 
 /**
  * @author <a href="mailto:xuender@gmail.com">ender</a>
- * 
+ *
  */
 public class UnidecodeTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
 
-	/**
-	 * Test method for
-	 * {@link me.xuender.unidecode.Unidecode#decode(java.lang.String)}.
-	 */
-	@Test
-	public void testDecode() {
-		assertEquals("hello world", "Hello world.",
-				Unidecode.decode("Hello world."));
-		assertEquals("南无阿弥陀佛", "Nan Wu A Mi Tuo Fo", Unidecode.decode("南无阿弥陀佛"));
-		assertEquals("Κνωσός", "Knosos", Unidecode.decode("Κνωσός"));
-		assertEquals("あみだにょらい", "amidaniyorai", Unidecode.decode("あみだにょらい"));
-	}
+    /**
+     * Test method for
+     * {@link me.xuender.unidecode.Unidecode#decode(java.lang.String)}.
+     */
+    @Test
+    public void testDecode() {
+        assertEquals("hello world", "Hello world.",
+                Unidecode.decode("Hello world."));
+        assertEquals("南无阿弥陀佛", "Nan Wu A Mi Tuo Fo", Unidecode.decode("南无阿弥陀佛"));
+        assertEquals("Κνωσός", "Knosos", Unidecode.decode("Κνωσός"));
+        assertEquals("あみだにょらい", "amidaniyorai", Unidecode.decode("あみだにょらい"));
+    }
 
-	/**
-	 * Test method for
-	 * {@link me.xuender.unidecode.Unidecode#decode(java.lang.String)}.
-	 */
-	@Test
-	public void testDecodeException() {
-		assertEquals("", "", Unidecode.decode(""));
-		assertEquals("NULL", "", Unidecode.decode(null));
-	}
+    /**
+     * Test method for
+     * {@link me.xuender.unidecode.Unidecode#decode(java.lang.String)}.
+     */
+    @Test
+    public void testDecodeException() {
+        assertEquals("", "", Unidecode.decode(""));
+        assertEquals("NULL", "", Unidecode.decode(null));
+    }
 
-	/**
-	 * Test method for
-	 * {@link me.xuender.unidecode.Unidecode#initials(java.lang.String)}.
-	 */
-	@Test
-	public void testInitials() {
-		assertEquals("hello world", "Hw", Unidecode.initials("Hello world."));
-		assertEquals("南无阿弥陀佛", "NWAMTF", Unidecode.initials("南无阿弥陀佛"));
-		assertEquals("Κνωσός", "K", Unidecode.initials("Κνωσός"));
-		assertEquals("あみだにょらい", "a", Unidecode.initials("あみだにょらい"));
-		assertEquals("enter", "XXGN\nQZQC\n\rTZHL\tSSC",
-				Unidecode.initials("小小姑娘\n清早起床\n\r提着花篮\t上市场。"));
-	}
+    /**
+     * Test method for
+     * {@link me.xuender.unidecode.Unidecode#initials(java.lang.String)}.
+     */
+    @Test
+    public void testInitials() {
+        assertEquals("hello world", "Hw", Unidecode.initials("Hello world."));
+        assertEquals("南无阿弥陀佛", "NWAMTF", Unidecode.initials("南无阿弥陀佛"));
+        assertEquals("Κνωσός", "K", Unidecode.initials("Κνωσός"));
+        assertEquals("あみだにょらい", "a", Unidecode.initials("あみだにょらい"));
+        assertEquals("enter", "XXGN\nQZQC\n\rTZHL\tSSC",
+                Unidecode.initials("小小姑娘\n清早起床\n\r提着花篮\t上市场。"));
+    }
 
-	/**
-	 * Test method for
-	 * {@link me.xuender.unidecode.Unidecode#initials(java.lang.String)}.
-	 */
-	@Test
-	public void testInitialsException() {
-		assertEquals("", "", Unidecode.initials(""));
-		assertEquals("NULL", "", Unidecode.initials(null));
-	}
+    /**
+     * Test method for
+     * {@link me.xuender.unidecode.Unidecode#initials(java.lang.String)}.
+     */
+    @Test
+    public void testInitialsException() {
+        assertEquals("", "", Unidecode.initials(""));
+        assertEquals("NULL", "", Unidecode.initials(null));
+    }
 
-	/**
-	 * 2013-09-06 17:57
-	 * 
-	 * 你好，最近在项目中使用Unidecode，把用户名转拼音后的声母取出来，发现在转换“一”这个字的时候结果有问题 String pinyin =
-	 * Unidecode.decode("一条会走路的鱼"); System.out.print(pinyin.charAt(0)); 输出结果为：[
-	 * 怎么办？
-	 */
-	@Test
-	public void testDecodeYi() {
-		assertEquals("何清宝提出的bug", "Yi Tiao Hui Zou Lu De Yu",
-				Unidecode.decode("一条会走路的鱼"));
-	}
+    /**
+     * 2013-09-06 17:57
+     *
+     * 你好，最近在项目中使用Unidecode，把用户名转拼音后的声母取出来，发现在转换“一”这个字的时候结果有问题 String pinyin =
+     * Unidecode.decode("一条会走路的鱼"); System.out.print(pinyin.charAt(0)); 输出结果为：[
+     * 怎么办？
+     */
+    @Test
+    public void testDecodeYi() {
+        assertEquals("何清宝提出的bug", "Yi Tiao Hui Zou Lu De Yu",
+                Unidecode.decode("一条会走路的鱼"));
+    }
 
-	/**
-	 * 检查全部汉字是否还有[开头的情况
-	 */
-	@Test
-	public void testAll() {
-		int a = (int) (4 * Math.pow(16, 3) + 14 * Math.pow(16, 2)); // 汉字ASCII码最小值
-		int b = (int) (9 * Math.pow(16, 3) + 15 * Math.pow(16, 2) + 10 * Math
-				.pow(16, 1)) + 5; // 汉字ASCII码最大值
-		for (int i = a; i <= b; i++) {
-			assertNotEquals("不是[开头", "[", Unidecode.decode((char) i + "")
-					.charAt(0));
-		}
-	}
+    /**
+     * 检查全部汉字是否还有[开头的情况
+     */
+    @Test
+    public void testAll() {
+        int a = (int) (4 * Math.pow(16, 3) + 14 * Math.pow(16, 2)); // 汉字ASCII码最小值
+        int b = (int) (9 * Math.pow(16, 3) + 15 * Math.pow(16, 2) + 10 * Math
+                .pow(16, 1)) + 5; // 汉字ASCII码最大值
+        for (int i = a; i <= b; i++) {
+            assertNotEquals("不是[开头", "[", Unidecode.decode((char) i + "")
+                    .charAt(0));
+        }
+    }
 
-	/**
-	 * ArrayIndexOutOfBound exception
-	 * 
-	 * Schweigi opened this issue
-	 * 
-	 * If decode() is used with e.g. an emoticon character there is a
-	 * ArrayIndexOutOfBound exception thrown.
-	 * 
-	 * Example text: http://www.scarfboy.com/coding/unicode-tool?s=U%2b1F61C
-	 * 
-	 * String[] ret = cache[section]; (Line: 63)
-	 * 
-	 * The reason is that on Line 52 int section = codepoint >> 8; the section
-	 * will be bigger than 255 and therefore out of the cache area.
-	 * 
-	 * In my opinion there are two solution:
-	 * 
-	 * Either the cache is made bigger to contain all value up to 0xEFFFF or
-	 * Characters with a value bigger (> 0xFFFF) than the cache should not be
-	 * cached.
-	 */
-	@Test
-	public void testIssues2() {
-		assertEquals("", Unidecode.decode("😜"));
-		assertEquals("H", Unidecode.decode("Ｈ"));
-	}
+    /**
+     * ArrayIndexOutOfBound exception
+     *
+     * Schweigi opened this issue
+     *
+     * If decode() is used with e.g. an emoticon character there is a
+     * ArrayIndexOutOfBound exception thrown.
+     *
+     * Example text: http://www.scarfboy.com/coding/unicode-tool?s=U%2b1F61C
+     *
+     * String[] ret = cache[section]; (Line: 63)
+     *
+     * The reason is that on Line 52 int section = codepoint >> 8; the section
+     * will be bigger than 255 and therefore out of the cache area.
+     *
+     * In my opinion there are two solution:
+     *
+     * Either the cache is made bigger to contain all value up to 0xEFFFF or
+     * Characters with a value bigger (> 0xFFFF) than the cache should not be
+     * cached.
+     */
+    @Test
+    public void testIssues2() {
+        assertEquals("", Unidecode.decode("😜"));
+        assertEquals("H", Unidecode.decode("Ｈ"));
+    }
 }
