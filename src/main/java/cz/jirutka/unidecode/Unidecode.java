bc51526b45a4d6b142c021e1825b121eb28bd2cd
Jakub Jirutka <jakub@jirutka.cz>
2014-04-24 23:09:01 +0200

    Add copyright header to sources

--- src/main/java/cz/jirutka/unidecode/Unidecode.java
+++ src/main/java/cz/jirutka/unidecode/Unidecode.java
@@ -1,5 +1,18 @@
-/**
+/*
+ * Copyright 2013 徐晨阳 <xuender@gmail.com>.
+ * Copyright 2014 Jakub Jirutka <jakub@jirutka.cz>.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package cz.jirutka.unidecode;
 
@@ -16,9 +29,6 @@ import java.util.regex.Pattern;
  * characters from the specified charset.
  *
  * @see <a href="https://pypi.python.org/pypi/Unidecode">Python's Unidecode</a>
-
- * @author <a href="mailto:xuender@gmail.com">ender</a>
- * @author Jakub Jirutka <jakub@jirutka.cz></jakub@jirutka.cz>
  */
 public class Unidecode {
 
